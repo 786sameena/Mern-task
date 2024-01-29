54ad6b3ec3af0b6e6cd02ae09c7c9c9915f7c80d
786sameena <687sameenamd@gmail.com>
2024-01-29 15:40:18 +0530

    first commit

---
+++ backend/model/schema.js
@@ -0,0 +1,15 @@
+// TaskModel.js
+
+const mongoose = require('mongoose');
+
+const formSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  age: String,
+  email: String,
+  phoneNumber: String,
+});
+
+const Form = mongoose.model('Form', formSchema);
+
+module.exports = Form;
